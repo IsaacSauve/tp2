e7753abb84ddfabcaa1712c517538d83d2864503
leaneLavigne <leane.lavigne@umontreal.ca>
2023-03-30 16:07:40 -0400

    Début javadoc

--- src/main/java/server/EventHandler.java
+++ src/main/java/server/EventHandler.java
@@ -1,6 +1,17 @@
 package server;
 
+/**
+ *L'interface fonctionnelle "EventHandler" est dédié à traîter la réaction du serveur à un évènement donné,
+ * soit handleLoadCourses et handleRegistration.
+ */
 @FunctionalInterface
 public interface EventHandler {
+    /**
+     * La méthode handle nous permet de manipuler la commande reçu.
+     *
+     * @param cmd   Une chaîne de caractères représentant la commande du client.
+     * @param arg   Une chaîne de caractères représentant pour quelle session le client souhaite que la commande
+     *              soit exécutée.
+     */
     void handle(String cmd, String arg);
 }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -10,15 +10,38 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * La classe serveur nous permet d'établir la connexion avec un client, d'attendre les commandes de
+ * celui-çi, de lister les différents cours disponibles selon la session désirée et
+ * d'exécuter l'inscription d'un étudiant.
+ */
 public class Server {
+
+    /**
+     * Chaînes de caractères permettant  de reconnaître quelle commande
+     * doit être exécuter.
+     */
 
     public final static String REGISTER_COMMAND = "INSCRIRE";
     public final static String LOAD_COMMAND = "CHARGER";
+
+    /**
+     * L'objet Socket va nous permettre d'ouvrir une connexion
+     * sur notre serveur.
+     */
     private final ServerSocket server;
     private Socket client;
     private ObjectInputStream objectInputStream;
     private ObjectOutputStream objectOutputStream;
     private final ArrayList<EventHandler> handlers;
+
+    /**
+     *Constructeur permettant l'initialisation de la connexion au serveur sur le port passé en paramètre, de créer le
+     *tableau allant contenir les "EventHandler" et ajouter ceux-ci dans le tableau "handlers".
+     *
+     * @param port  Port sur lequel le serveur va être créé.
+     * @throws IOException  Erreur d'entrée/sortie en générale.
+     */
 
     public Server(int port) throws IOException {
         this.server = new ServerSocket(port, 1);
@@ -26,16 +49,35 @@ public class Server {
         this.addEventHandler(this::handleEvents);
     }
 
+    /**
+     * Cette méthode ajoute l'EventHandler mis en paramètre à l'ArrayList nommé "handlers".
+     *
+     * @param h     L'EventHandler qui doit être ajouté à l'ArrayList "handlers".
+     */
     public void addEventHandler(EventHandler h) {
         this.handlers.add(h);
     }
 
+    /**
+     * Cette méthode informe les "EventHandlers" que la commande reçu en paramètre doit être traîtée.
+     *
+     * @param cmd   Une chaîne de caractères représentant la commande du client.
+     * @param arg   Une chaîne de caractères représentant pour quelle session le client souhaite que la commande
+     *              soit exécutée.
+     */
     private void alertHandlers(String cmd, String arg) {
         for (EventHandler h : this.handlers) {
             h.handle(cmd, arg);
         }
     }
 
+    /**
+     * Méthode qui attend qu'une connexion soit établit avec un client, pour ensuite afficher des informations sur le
+     * client, recevoir des données de celui-ci et en renvoyer en fonction des données reçues par le client.
+     * Ce processus va continuer tant que le client est connecté.
+     * Dès que le client se déconnecte, on affiche la chaîne de caractère "Client déconnecté" et le serveur se ferme.
+     * De plus, si une exception survient, elle est attrapée.
+     */
     public void run() {
         while (true) {
             try {
@@ -52,6 +94,14 @@ public class Server {
         }
     }
 
+    /**
+     * Méthode permettant le démarrage de la boucle d'évènement en attendant qu'une connexion avec un client soit
+     * établit.
+     *
+     * @throws IOException  Erreur d'entrée/sortie en générale.
+     * @throws ClassNotFoundException   La classe demandé n'a pas pu être trouvé. (+++)
+     */
+
     public void listen() throws IOException, ClassNotFoundException {
         String line;
         if ((line = this.objectInputStream.readObject().toString()) != null) {
@@ -62,6 +112,18 @@ public class Server {
         }
     }
 
+    /**
+     * Méthode qui va  créer le tableau "parts" avec la chaîne de caractères reçu en argument, où chaque case
+     * va contenir un mot de cette chaîne, pour par la suite déclarer(?) la chaîne de caractère "cmd" par le mot à
+     * l'index 0 du tableau "parts" et joindre toutes les cases sauf celle à l'index 0, avec des espaces entre chaque
+     * mot qui va être utilisé pour déclarer la chaîne de caractère "args".
+     * La méthode se termine en retournant une nouvelle paire(?) avec "cmd" et "args" en paramètre.
+     *
+     * @param line Chaîne de caractères reçu par le client que l'on doit interpréter.
+     * @return  La commande qui doit être exécutée et la session pour laquelle la commande va etre exécutée.
+     *
+     * ??? Est-ce que args peut être autre chose que des session???
+     */
     public Pair<String, String> processCommandLine(String line) {
         String[] parts = line.split(" ");
         String cmd = parts[0];
