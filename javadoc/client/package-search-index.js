fea4ccc9a3766e11e3f6151984dd738bfaa70a19
IsaacSauve <isaacsauve@hotmail.com>
2023-04-12 22:13:37 -0400

    fin javadoc

--- javadoc/client/package-search-index.js
+++ javadoc/client/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"client"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"mvc"}];updateSearchResults();
--- javadoc/client/type-search-index.js
+++ javadoc/client/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"client","l":"Client"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"mvc","l":"Controleur"}];updateSearchResults();
